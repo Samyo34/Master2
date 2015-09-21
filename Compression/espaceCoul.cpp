376758b1c1fb7b1c6cc36420886d9ff07dae885a
Samyo34 <sambricas@hotmail.com>
2015-09-21 14:46:01 +0200

    debut agrandissment

--- Compression/espaceCoul.cpp
+++ Compression/espaceCoul.cpp
@@ -9,11 +9,11 @@ int getIndex(int x, int y, int imgW){
 }
 
 void reduc(OCTET *in, OCTET *out, int lignes, int colonnes, int color1, int color2) {
-	int i, j, k, index, index2;
-	for(i = 0; i < lignes; i++){
-		for(j = 0; j < colonnes; j++){
+	int index, index2;
+	for(int i = 0; i < lignes; i++){
+		for(int j = 0; j < colonnes; j++){
 			index = getIndex(i * 3, j, lignes*3);
-			for(k = 0; k < 3; k++){
+			for(int k = 0; k < 3; k++){
 				if(k == color1 || k == color2){
 					if(i <= lignes/2 && j <= colonnes/2){
 						out[index+k] = in[getIndex(i * 3 * 2, j * 2, lignes*3)];
@@ -30,7 +30,18 @@ void reduc(OCTET *in, OCTET *out, int lignes, int colonnes, int color1, int colo
 
 
 void agrandir(OCTET* in, OCTET *out, int lignes, int colonnes, int color1, int color2){
-	
+	for(int i=0;i<lignes;i++){
+		for(int j=0;j<colonnes;j++){
+			for(int k=0;k<3;k++){
+				if(i<=lignes/2 && j<=colonnes/2){
+					out[j*colonnes*3+(i*3)+k]=0;
+				}else{
+					out[j*colonnes*3+(i*3)+k]=255;
+				}
+			}
+			
+		}
+	}
 }
 
 
@@ -38,13 +49,14 @@ void agrandir(OCTET* in, OCTET *out, int lignes, int colonnes, int color1, int c
 int main(int argc, char* argv[]){
 	char cNomImgLue[250];
 	char reduction[250] = "reduite.ppm";
+	char agr[250] = "agrandi.ppm";
 	int lignes, colonnes, nTaille, S;
 	if (argc != 2) {
 		printf("Usage: ImageIn.ppm\n");
 		exit (1) ;
 	}
 	sscanf (argv[1],"%s",cNomImgLue);
-	OCTET *ImgIn, *ImgOut;
+	OCTET *ImgIn, *ImgOut, *ImgOut2;
 	lire_nb_lignes_colonnes_image_ppm(cNomImgLue, &lignes, &colonnes);
 	nTaille = lignes * colonnes;
 
@@ -53,9 +65,13 @@ int main(int argc, char* argv[]){
 
 	allocation_tableau(ImgOut, OCTET, nTaille*3);
 
+	allocation_tableau(ImgOut2, OCTET, nTaille*3)
+
 	reduc(ImgIn,ImgOut,lignes,colonnes,0,2);
+	agrandir(ImgOut,ImgOut2,lignes,colonnes,0,2);
 
 	ecrire_image_ppm(reduction, ImgOut, lignes, colonnes);
+	ecrire_image_ppm(agr, ImgOut2, lignes, colonnes);
 	free(ImgIn);
 	return 1;
 }
